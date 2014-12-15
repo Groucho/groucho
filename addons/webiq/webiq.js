278a9e417f76efa1e259ce6e548e1fbfcf1dd116
Josh Lind <jlind@tableausoftware.com>
2014-12-14 23:48:33 -0800

    update draft

--- addons/webiq/webiq.js
+++ addons/webiq/webiq.js
@@ -3,113 +3,174 @@
  * Determine user's comfort with web norms. Cough.
  */
 
-(function ($) {
+groucho.addons = groucho.addons || {};
+groucho.config.addons = groucho.config.addons || {};
 
-	// Setup.
-  window.groucho = window.groucho || {};
-	groucho.config = groucho.config || {};
-	groucho.config.addons = groucho.config.addons || {};
-	// Defaults.
-  groucho.config.addons.webiq = {
-    'startScore': 0,
+(function ($, groucho) {
+
+  // Defaults.
+  groucho.config.addons.webIQ = {
     'behaviors': {
-      'formsDelete': { 'increment': '-1', 'theshold': 3, 'stash': 0 },
-      'formsTab': { 'increment': '2' },
-      'findOnPage': { 'increment': '4' },
-      'doubleClick': { 'increment': '.5' }
-      'repeatPage': { 'increment': '3' }
+      'formsDelete': { 'increment': -1, 'theshold': 3, 'stash': 0 },
+      'formsTab': { 'increment': 2 },
+      'findOnPage': { 'increment': 4 },
+      'doubleClick': { 'increment': 0.5 },
+      'repeatPage': { 'increment': 3, 'theshold': 1 }
     }
   };
 
   // React to page load.
   $(document).ready(function () {
+    behaviors = groucho.config.addons.webIQ.behaviors;
+
     // Call all behavior functions.
-    for (var i in groucho.config.addons.webiq.behaviors) {
-      groucho.addons.webiq[groucho.config.addons.webiq.behavior[i]].call();
+    for (var i in behaviors) {
+      groucho.addons.webIQ[behaviors[i]].call();
     }
   });
 
+  groucho.addons.webIQ = {
 
-  /**
-   * How good is this user at internetting.
-   */
-  groucho.addons.webiq.getScore = function () {
-    var results = groucho.getActivities('webiq'),
-    score = groucho.config.addons.webiq.startScore;
-    // Add it up for better or worse.
-    for (var i in results) {
-      score = score + groucho.config.addons.webiq.behaviors[results[i].type].increment;
+    /**
+     * How good is this user at internetting.
+     *
+     * @return int|object
+     *   Score value or behaviors
+     */
+    getScore: function getScore(behavior) {
+      var results = groucho.getActivities('webIQ'),
+          score;
+
+      // Return options.
+      if (behavior === undefined) {
+        score = 0;
+        // Add it up for better or worse.
+        for (var i in results) {
+          score = score + groucho.config.addons.webIQ.behaviors[results[i].type].increment;
+        }
+      }
+      else if (behavior === '*') {
+        score = {};
+        // Segment by behavior type.
+        for (i in results) {
+          scores[behavior] = scores[behavior] + groucho.config.addons.webIQ.behaviors[behavior].increment;
+        }
+      }
+      else {
+        score = 0;
+        // Just the one behavior type.
+        for (var i in results) {
+          if (results[i].type === behavior) {
+            score = score + groucho.config.addons.webIQ.behaviors[behavior].increment;
+          }
+        }
+      }
+
+      return score;
+    },
+
+
+    /**
+    * Utility: Confirm threshold before recording.
+    *
+    * @param string behavior
+    *   Behavior being monitored.
+    * @param int count
+    *   Current threshold count.
+    *
+    * @return boolean
+    *   Results of check.
+    */
+    checkThreshold: function checkThreshold(behavior, count) {
+      if (count >= groucho.config.addons.webIQ.behaviors[behavior].threshold) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    },
+
+    /**
+     * Listen: Delete keypresses in forms.
+     */
+    formsDelete: function formsDelete() {
+      $('input').keypress(function (e) {
+        if (e.which === 8 || e.which === 46) {
+          // Allow a threshold, on-page.
+          if (checkThreshold(this.name, groucho.config.addons.webIQ.behaviors[this.name].stash)) {
+            groucho.createActivity('webIQ', { 'type': this.name });
+          }
+          else {
+            groucho.config.addons.webIQ.behaviors[this.name].stash++;
+          }
+
+console.log("Executed: " + this.name);
+
+          }
+        }
+      });
+    },
+
+    /**
+     * Listen: Use of delete in forms.
+     */
+    formsTab: function formsTab() {
+      $('input').keypress(function (e) {
+        if (e.which === 9) {
+          groucho.createActivity('webIQ', { 'type': this.name });
+
+console.log("Executed: " + this.name);
+
+        }
+      });
+    },
+
+    /**
+     * Listen: Typing speed in forms.
+     */
+    findOnPage: function findOnPage() {
+      $(document).keypress('f', function (e) {
+        if (e.ctrlKey)
+          groucho.createActivity('webIQ', { 'type': this.name });
+
+console.log("Executed: " + this.name);
+
+      });
+    },
+
+    /**
+     * Listen: Double clicking, evar.
+     */
+    doubleClick: function doubleClick() {
+      $(document).dblclick(function() {
+        groucho.createActivity('webIQ', { 'type': this.name });
+
+console.log("Executed: " + this.name);
+
+      });
+    },
+
+
+    /**
+     * Listen: Clicking a link to the same page.
+     */
+    repeatPage: function repeatPage() {
+      $('a').click(function() {
+        if (this.href = location.href) {
+          // Allow a threshold, persistent.
+          if (checkThreshold(this.name, groucho.getActivities('webIQ:' + this.name).length)) {
+            groucho.createActivity('webIQ', { 'type': this.name });
+          }
+          else {
+            groucho.createActivities('webIQ:' + this.name);
+          }
+
+console.log("Executed: " + this.name);
+
+        }
+      });
     }
-    return score;
+
   };
 
-
-  /**
-   * Listen: Delete keypresses in forms.
-   */
-  groucho.addons.webiq.formsDelete = function () {
-    $('input').keypress(function (e) {
-      if (e.which === 8 || e.which === 46) {
-        groucho.createActivity('webiq', { 'type': this.name });
-
-console.log("Executed: " + this.name);
-
-      }
-    });
-  };
-
-  /**
-   * Listen: Use of delete in forms.
-   */
-  groucho.addons.webiq.formsTab = function () {
-    $('input').keypress(function (e) {
-      if (e.which === 9) {
-        groucho.createActivity('webiq', { 'type': this.name });
-
-console.log("Executed: " + this.name);
-
-      }
-    });
-  };
-
-  /**
-   * Listen: Typing speed in forms.
-   */
-  groucho.addons.webiq.findOnPage = function () {
-    $(document).keypress('f', function (e) {
-      if (e.ctrlKey)
-        groucho.createActivity('webiq', { 'type': this.name });
-
-console.log("Executed: " + this.name);
-
-    });
-  };
-
-  /**
-   * Listen: Double clicking, evar.
-   */
-  groucho.addons.webiq.doubleClick = function () {
-    $(document).dblclick(function() {
-      groucho.createActivity('webiq', { 'type': this.name });
-
-console.log("Executed: " + this.name);
-
-    });
-  };
-
-
-  /**
-   * Listen: Clicking a link to the same page.
-   */
-  groucho.addons.webiq.repeatPage = function () {
-    $('a').click(function() {
-      if (this.href = location.href) {
-        groucho.createActivity('webiq', { 'type': this.name });
-      }
-
-console.log("Executed: " + this.name);
-
-    });
-  };
-
- })(jQuery);
+ })(jQuery, groucho);
