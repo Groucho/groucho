872f3f8ebf8b6964df97ab89bc0fbd9f15db6a31
Trevor <trevor.m.fox@gmail.com>
2017-08-15 12:10:25 -0700

    A couple edits for consistency
    used hasOwnProperty rather than comparison, made it easier to read

--- src/groucho.js
+++ src/groucho.js
@@ -22,14 +22,14 @@ var groucho = window.groucho || {};
         'addons': {}
       };
 
-  // If config property is not set, use defaults.
-  if (groucho.config === undefined){
-       groucho.config = groucho.defaults;
+  // Handle empty config.
+  if (!groucho.hasOwnProperty(config) {
+    groucho.config = groucho.defaults;
   } else {
-    // Else set empty configs to defaults.
-    for (var config in defaults) {
-      if (!groucho.config.hasOwnProperty(config)) {
-        groucho.config[config] = defaults[config];
+    // Set empty configs to defaults.
+    for (var setting in defaults) {
+      if (!groucho.config.hasOwnProperty(setting)) {
+        groucho.config[setting] = defaults[setting];
       }
     }
   }
